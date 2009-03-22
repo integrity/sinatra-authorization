f5d65708d7ea06edc5d9a87e5bf1b882465e8f19
Simon Rozet <simon@rozet.name>
2009-03-22 01:30:02 +0100

    Is it more Sinatra-ish?
    BTW, I'd like to bloat sinatra-authorization
    with all possibles DSL extensions/options/whatever
    so that we can figure what is/are the best way(s).

--- lib/sinatra/authorization.rb
+++ lib/sinatra/authorization.rb
@@ -13,75 +13,79 @@ module Sinatra
   # Code adapted from {Ryan Tomayko}[http://tomayko.com/about] and
   # {Christopher Schneid}[http://gittr.com], shared under an MIT License
   module Authorization
-    # Redefine this method on your helpers block to actually contain
-    # your authorization logic.
-    def authorize(username, password)
-      false
-    end
-
-    # From you app, call set :authorization_realm, "my app" to set this
-    # or define a #authorization_realm method in your helpers block.
-    def authorization_realm
-      Sinatra::Default.authorization_realm
-    end
-
-    # Call in any event that requires authentication
-    def login_required
-      return if authorized?
-      unauthorized! unless auth.provided?
-      bad_request!  unless auth.basic?
-      unauthorized! unless authorize(*auth.credentials)
-      request.env['REMOTE_USER'] = auth.username
-    end
-
-    # Convenience method to determine if a user is logged in
-    def authorized?
-      !!request.env['REMOTE_USER']
-    end
-    alias :logged_in? :authorized?
-
-    # Name provided by the current user to log in
-    def current_user
-      request.env['REMOTE_USER']
-    end
-
-    private
-      def auth
-        @auth ||= Rack::Auth::Basic::Request.new(request.env)
+    module Helpers
+      # Redefine this method on your helpers block to actually contain
+      # your authorization logic.
+      def authorize(username, password)
+        false
       end
 
-      def unauthorized!(realm=authorization_realm)
-        response["WWW-Authenticate"] = %(Basic realm="#{realm}")
-        throw :halt, [ 401, 'Authorization Required' ]
+      # From you app, call set :authorization_realm, "my app" to set this
+      # or define a #authorization_realm method in your helpers block.
+      def authorization_realm
+        Sinatra::Default.authorization_realm
       end
 
-      def bad_request!
-        throw :halt, [ 400, 'Bad Request' ]
+      # Call in any event that requires authentication
+      def login_required
+        return if authorized?
+        unauthorized! unless auth.provided?
+        bad_request!  unless auth.basic?
+        unauthorized! unless authorize(*auth.credentials)
+        request.env['REMOTE_USER'] = auth.username
       end
 
-    module ClassMethods
+      # Convenience method to determine if a user is logged in
+      def authorized?
+        !!request.env['REMOTE_USER']
+      end
+      alias :logged_in? :authorized?
+
+      # Name provided by the current user to log in
+      def current_user
+        request.env['REMOTE_USER']
+      end
+
+      private
+        def auth
+          @auth ||= Rack::Auth::Basic::Request.new(request.env)
+        end
+
+        def unauthorized!(realm=authorization_realm)
+          response["WWW-Authenticate"] = %(Basic realm="#{realm}")
+          throw :halt, [ 401, 'Authorization Required' ]
+        end
+
+        def bad_request!
+          throw :halt, [ 400, 'Bad Request' ]
+        end
+    end
+
+    module DSL
       def protecting(*patterns)
         @protected_routes = []
         @protected_routes += patterns
       end
 
-      def authorize(realm='app', &block)
-        define_method(:authorize, &block)
-        define_method(:authorization_realm) { realm }
-      end
-
       def protecting?(path)
         @protected_routes && @protected_routes.any? { |pattern| path =~ pattern }
       end
+
+      def authorize(realm="app", &block)
+        set :authorization_realm, realm
+        define_method(:authorize, &block)
+      end
+    end
+
+    def self.registered(app)
+      Sinatra.register(DSL)
+      app.helpers Helpers
+
+      app.before do
+        login_required if self.class.protecting?(request.path_info)
+      end
     end
   end
 
-  helpers do
-    include Authorization
-    extend Authorization::ClassMethods
-
-    before do
-      login_required if self.class.protecting?(request.path_info)
-    end
-  end
+  register Authorization
 end
